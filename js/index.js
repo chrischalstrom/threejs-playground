480f4b5dd53c1486fecca12ebe4f652c39ff4858
Chris Chalstrom <chris.chalstrom@gmail.com>
2014-12-30 16:01:03 -0700

    Make cube move by using THREEx.KeyboardState

--- js/index.js
+++ js/index.js
@@ -8,12 +8,12 @@ $(document).ready(function() {
   var clock = new THREE.Clock();
   var stats = addStatsToDom();
 
-  var keyboard = new KeyboardState();
+  var keyboard = new THREEx.KeyboardState();
 
   var sceneObjs = setupScene(viewAngle, screenWidth, screenHeight, near, far);
+  var worldObjs = createWorld(sceneObjs.scene);
 
-  createWorld(sceneObjs.scene);
-  animate(sceneObjs, keyboard, stats);
+  animate(sceneObjs, worldObjs, keyboard, clock, stats);
 
   function setupScene(viewAngle, width, height, near, far) {
     var scene = new THREE.Scene();
@@ -84,6 +84,10 @@ $(document).ready(function() {
     cube.castShadow = true;
     cube.position.set(100, 50, 100);
     scene.add(cube);
+
+    return {
+      cube: cube
+    };
   }
 
   function sceneLights() {
@@ -104,22 +108,27 @@ $(document).ready(function() {
     return lights;
   }
 
-  function animate(sceneObjs, keyboard, stats) {
-    requestAnimationFrame(function() { animate(sceneObjs, keyboard, stats) });
+  function animate(sceneObjs, worldObjs, keyboard, clock, stats) {
+    requestAnimationFrame(function() { animate(sceneObjs, worldObjs, keyboard, clock, stats) });
     render(sceneObjs);
-    update(sceneObjs, keyboard, stats);
+    update(worldObjs, keyboard, clock, stats);
   }
 
   function render(sceneObjs) {
     sceneObjs.renderer.render(sceneObjs.scene, sceneObjs.camera);
   }
 
-  function update(sceneObjs, keyboard, stats) {
+  function update(worldObjs, keyboard, clock, stats) {
+    var delta = clock.getDelta(); // seconds since last update
+    var moveDistance = delta * 100;
+
     if(keyboard.pressed('right')) {
-      alert('right');
+      worldObjs.cube.translateX(moveDistance);
+    }
+    else if(keyboard.pressed('left')) {
+      worldObjs.cube.translateX(-moveDistance);
     }
 
-    sceneObjs.orbitControls.update();
     stats.update();
   }
 
